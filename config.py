c940fd2d2b72e0a4433f196e5f2c8b3fc63d5c9e
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-09 23:59:47 +0530

    Update upstream repository and branch names

--- config.py
+++ config.py
@@ -55,9 +55,9 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RDXSHUKLA/SHUKLA",
+    "https://github.com/BadMundaX/Mode",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # ----------------------------------------------------------------
