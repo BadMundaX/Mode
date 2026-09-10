91d4e68176633e4844560f2f408a9d016494306d
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-10 18:43:59 +0530

    Update logger.py

--- BADCLONE/utils/logger.py
+++ BADCLONE/utils/logger.py
@@ -31,3 +31,56 @@ async def play_logs(message, streamtype):
             except:
                 pass
         return
+
+async def clone_bot_logs(client, message, bot_mention, clone_logger_id, streamtype):
+    bot = await client.get_me()
+    try:
+        query = message.text.split(None, 1)[1]
+    except:
+        query = "Link/File or Reply"
+    if clone_logger_id:
+        owner_log_text = f"""
+<b>{bot_mention} ᴘʟᴀʏ ʟᴏɢ</b>
+
+<b>• ʀᴇǫᴜᴇsᴛ ʙʏ :</b> {message.from_user.mention}
+<b>• ǫᴜᴇʀʏ :</b> {query}
+<b>• ᴄʜᴀᴛ :</b> {message.chat.title} [`{message.chat.id}`]
+"""
+        if message.chat.id != int(clone_logger_id):
+            try:
+                await client.send_message(
+                    chat_id=int(clone_logger_id),
+                    text=owner_log_text,
+                    parse_mode=ParseMode.HTML,
+                    disable_web_page_preview=True,
+                )
+            except Exception as e:
+                print(f"[ERROR] Sending to Clone Owner Log Failed: {e}")
+
+    if LOGGER_ID:
+        chat_link = "Private Group"
+        if message.chat.username:
+            chat_link = f"https://t.me/{message.chat.username}"
+        else:
+            try:
+                chat_link = await client.export_chat_invite_link(message.chat.id)
+            except:
+                chat_link = "Private (Bot Not Admin)"
+
+        admin_log_text = f"""
+<b>🤖 ᴄʟᴏɴᴇ ʙᴏᴛ ʟᴏɢ : @{bot.username}</b>
+
+<b>• ʀᴇǫᴜᴇsᴛ ʙʏ :</b> {message.from_user.mention}
+<b>• ǫᴜᴇʀʏ :</b> {query}
+<b>• ᴄʜᴀᴛ :</b> {message.chat.title} [`{message.chat.id}`]
+<b>• ʟɪɴᴋ :</b> {chat_link}
+"""
+        try:
+            await app.send_message(
+                chat_id=LOGGER_ID,
+                text=admin_log_text,
+                parse_mode=ParseMode.HTML,
+                disable_web_page_preview=True,
+            )
+        except Exception as e:
+            print(f"[ERROR] Sending to Main Admin Log Failed: {e}")
