2ffe6fc2769cfc1ca4f2800f860ecd0d9a8ba506
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-13 23:05:07 +0530

    Change SHRUTI_API_KEY in Youtube.py
    Updated SHRUTI_API_KEY to a new value.

--- BADCLONE/platforms/Youtube.py
+++ BADCLONE/platforms/Youtube.py
@@ -10,7 +10,7 @@ from py_yt import VideosSearch, Playlist
 import aiohttp
 
 API_URL = os.environ.get("SHRUTI_API_URL", "https://api.shrutibots.site")
-API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBots88hgntDhLBAxmui2bE72")
+API_KEY = os.environ.get("SHRUTI_API_KEY", "ShrutiBots1JyNWUFBwhFiouHmUyXC")
 DOWNLOAD_DIR = "downloads"
 
 def time_to_seconds(time):
