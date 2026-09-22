481ae8da56c57e40f2d7199a613d394502ab4af4
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-22 14:48:37 +0530

    Update extras.py

--- BADCLONE/utils/inline/extras.py
+++ BADCLONE/utils/inline/extras.py
@@ -1,13 +1,24 @@
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from config import SUPPORT_CHAT
+import random
+from pyrogram.enums import ButtonStyle
+
+
+def random_style():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
+
 
 
 def botplaylist_markup(_):
     buttons = [
         [
-            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT),
-            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text=_["S_B_9"], url=SUPPORT_CHAT, style=random_style()),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close", style=random_style()),
         ],
     ]
     return buttons
@@ -20,7 +31,7 @@ def close_markup(_):
                 InlineKeyboardButton(
                     text=_["CLOSE_BUTTON"],
                     callback_data="close",
-                ),
+                 style=random_style()),
             ]
         ]
     )
@@ -34,8 +45,9 @@ def supp_markup(_):
                 InlineKeyboardButton(
                     text=_["S_B_9"],
                     url=SUPPORT_CHAT,
-                ),
+                 style=random_style()),
             ]
         ]
     )
     return upl
+    
