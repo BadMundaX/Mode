796c530c822522e899f0c2a45aa7916ae0f5a6f3
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-22 14:47:42 +0530

    Update song.py

--- BADCLONE/utils/inline/song.py
+++ BADCLONE/utils/inline/song.py
@@ -1,5 +1,16 @@
 
 from pyrogram.types import InlineKeyboardButton
+import random
+from pyrogram.enums import ButtonStyle
+
+
+def random_style():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
+
 
 
 def song_markup(_, vidid):
@@ -8,16 +19,17 @@ def song_markup(_, vidid):
             InlineKeyboardButton(
                 text=_["SG_B_2"],
                 callback_data=f"song_helper audio|{vidid}",
-            ),
+             style=random_style()),
             InlineKeyboardButton(
                 text=_["SG_B_3"],
                 callback_data=f"song_helper video|{vidid}",
-            ),
+             style=random_style()),
         ],
         [
             InlineKeyboardButton(
                 text=_["CLOSE_BUTTON"], callback_data="close"
-            ),
+            , style=random_style()),
         ],
     ]
     return buttons
+    
