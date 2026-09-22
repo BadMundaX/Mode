34ab101221b5d162ce43b8da5d14d7b0d4688699
BadMundaX <singhsukhwinder07990@gmail.com>
2026-09-22 14:45:20 +0530

    Update start.py

--- BADCLONE/utils/inline/start.py
+++ BADCLONE/utils/inline/start.py
@@ -3,6 +3,17 @@ from pyrogram.types import InlineKeyboardButton, Message
 import config
 import asyncio
 from BADCLONE import app
+import random
+from pyrogram.enums import ButtonStyle
+
+
+def random_style():
+    return random.choice([
+        ButtonStyle.SUCCESS,
+        ButtonStyle.DANGER,
+        ButtonStyle.PRIMARY
+    ])
+
 
 
 # Start panel for inline buttons
@@ -11,8 +22,8 @@ def start_panel(_):
         [
             InlineKeyboardButton(
                 text=_["SO_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
-            ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            , style=random_style()),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=random_style()),
         ],
     ]
     return buttons
@@ -25,15 +36,15 @@ def private_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_3"],
                 url=f"https://t.me/{app.username}?startgroup=true",
-            )
+             style=random_style())
         ],
-        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
+        [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper", style=random_style())],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL, style=random_style()),
+            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT, style=random_style()),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID, style=random_style()),
         ],
     ]
     return buttons
